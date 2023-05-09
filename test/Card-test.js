7b51872283d50d88c0ce8af6e0c1130c4705b4a5
Jade Shi <ziyushi1997@gmail.com>
2023-05-09 00:34:40 -0700

    Add test on turn

--- test/Card-test.js
+++ test/Card-test.js
@@ -16,4 +16,16 @@ describe('card', function() {
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
     expect(card.correctAnswer).to.equal('object');
   });  
+
+  describe('turn', function() {
+    it('should be a function', function() {
+      expect(evaluateGuess).to.be.a('function');
+    });
+
+    it('should return if a guess to a flashcard question is correct or incorrect', function() {
+      const card = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter'); 
+      expect(evaluateGuess('pug', 'sea otter')).to.equal('incorrect');
+      expect(evaluateGuess('sea otter', 'sea otter')).to.equal('correct');
+    });
+  });
 });
