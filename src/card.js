3e6501d1315966932a72815107d8edbbeb1b4913
Jade Shi <ziyushi1997@gmail.com>
2023-05-09 23:58:58 -0700

    Add round related tests and functions

--- src/card.js
+++ src/card.js
@@ -16,7 +16,59 @@ function evaluateGuess(guess, correctAnswer) {
   }
 }
 
+function createDeck(cards) {
+  return cards;
+}
+
+function countCards(cards) {
+  let cardCount = cards.length;
+  return cardCount;
+}
+
+function createRound(deck) {
+  let round = {
+    deck: deck,
+    currentCard: deck[0],
+    turns: 0,
+    incorrectGuesses: []
+  };
+  return round;
+}
+
+function takeTurn(guess, round) {
+  let message; 
+  if (guess === round.currentCard.correctAnswer) {
+    message = 'correct';
+  } else {
+    round.incorrectGuesses.push(round.currentCard.id);
+    message = 'incorrect';
+  }
+ 
+  round.turns += 1;
+  let index = round.turns;
+  round.currentCard = round.deck[index];
+  
+  return message;
+}
+
+function calculatePercentCorrect(round) {
+  let wrongGuesses = round.incorrectGuesses.length;
+  let percentage = (round.turns - wrongGuesses) / round.turns * 100;
+  return percentage;
+}
+
+function endRound(round) {
+  let percentage = calculatePercentCorrect(round);
+  console.log(`** Round over! ** You answered <${percentage}>% of the questions correctly!`)
+}
+
 module.exports = {
   createCard,
-  evaluateGuess
+  evaluateGuess,
+  createDeck,
+  countCards,
+  createRound,
+  takeTurn,
+  calculatePercentCorrect,
+  endRound
 }
