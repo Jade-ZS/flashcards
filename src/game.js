78a81dc4572765faa5bd74e80dc79664aadaf27e
Jade Shi <ziyushi1997@gmail.com>
2023-05-10 13:25:41 -0700

    Add start

--- src/game.js
+++ src/game.js
@@ -11,4 +11,23 @@ function printQuestion(round) {
   util.main(round);
 }
 
+function start() {
+    let cards = getCards([]);
+    let deck = createDeck(cards);
+    let round = createRound(deck);
+    printMessage(deck);
+    printQuestion(round);
+}
+
+function getCards(cards) {
+  cards.push(data[Math.floor(Math.random() * data.length)]);
+  if (cards.length > 4) {
+    console.log(cards)
+    return cards;
+  }
+  return getCards(cards);
+}
+
+start();
+
 module.exports = { printMessage, printQuestion };
